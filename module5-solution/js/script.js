9706996cc8979bb6db137c2123cef977dcc29820
Vaibhavi Rane <vaibhavirane2000@gmail.com>
2020-08-16 13:16:33 +0530

    Module 5 solution

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -13,15 +13,15 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 var dc = {};
 
-var homeHtmlUrl = "C:/Users/Vaibhavi/Desktop/coursera-test/module5-solution/snippets/home-snippet.html";
+var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl =
   "https://davids-restaurant.herokuapp.com/categories.json";
-var categoriesTitleHtml = "C:/Users/Vaibhavi/Desktop/coursera-test/module5-solution/snippets/categories-title-snippet.html";
-var categoryHtml = "C:/Users/Vaibhavi/Desktop/coursera-test/module5-solution/snippets/categories-snippet.html";
+var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
   "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-var menuItemsTitleHtml = "C:/Users/Vaibhavi/Desktop/coursera-test/module5-solution/snippets/menu-items-title.html";
-var menuItemHtml = "C:/Users/Vaibhavi/Desktop/coursera-test/module5-solution/snippets/menu-item.html";
+var menuItemsTitleHtml = "snippets/menu-items-title.html";
+var menuItemHtml = "snippets/menu-item.html";
 
 // Convenience function for inserting innerHTML for 'select'
 var insertHtml = function (selector, html) {
@@ -32,7 +32,7 @@ var insertHtml = function (selector, html) {
 // Show loading icon inside element identified by 'selector'.
 var showLoading = function (selector) {
   var html = "<div class='text-center'>";
-  html += "<img src='C:/Users/Vaibhavi/Desktop/coursera-test/module5-solution/images/ajax-loader.gif'></div>";
+  html += "<img src='images/ajax-loader.gif'></div>";
   insertHtml(selector, html);
 };
 
